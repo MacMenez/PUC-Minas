2408d55fd7ff419f4288b947ec3a4575c58b5327
David Ram Menez <dcm.facul@gmail.com>
2023-01-31 03:28:40 -0300

    Cap.3
    Atualizando README e continuando com projetos desenvolvidos em sala.

---
+++ ALGORITMOS-E-ESTRUTURAS-DE-DADOS-I/3 - Entrada e Saída/1 - Saida_Formatada/src/Saida_Formatada.java
@@ -0,0 +1,8 @@
+public class Saida_Formatada {
+    public static void main(String[] args) throws Exception {
+        int quantidade = 1;
+        double veloc = 55.5;
+        
+        System.out.printf ("O carro possui %d portas e sua velocidade é %2.1f km/h", quantidade, veloc);
+    }
+}
