02d9ce77239cbacfb3f003897f71e017dbd18901
David Ram Menez <dcm.facul@gmail.com>
2023-02-11 00:50:41 -0300

    Inserindo Aulas de Introdução
    Linguagens C, C++, C#, JAVA e Python

---
+++ ALGORITMOS-E-ESTRUTURAS-DE-DADOS-II/0 - Revisao/2 - Tipos de Dados/tipoDeDados.cs
@@ -0,0 +1,8 @@
+/*
+sbyte, short, int e long - Inteiro com Sinal
+byte, ushort, uint, e ulong - Inteiro sem Sinal
+char - caractere
+float, double e decimal - Ponto Flutuante
+bool - Boleano
+String - cadeia de caracteres(vetor)
+*/
---
+++ ALGORITMOS-E-ESTRUTURAS-DE-DADOS-II/0 - Revisao/2 - Tipos de Dados/tiposDeDados.cpp
@@ -0,0 +1,14 @@
+/*
+char - caractere
+int - inteiro
+float - ponto flutuante
+double - ponto flutuante
+bool - boleano
+char[] - cadeia de carcteres(vetor)
+
+C possue os modificadores de tipos:
+short
+long
+unsigned
+signed
+*/
