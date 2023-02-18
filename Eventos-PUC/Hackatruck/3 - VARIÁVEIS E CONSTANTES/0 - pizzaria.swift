008556b234a2f0782bc98c8fbb8c767eaa3621d5
David Ram Menez <dcm.facul@gmail.com>
2023-02-18 11:03:13 -0300

    Testando Variáveis em SWIFT
    Programa de treinamento do Evento Hackatruck PUC Minas 2023.

---
+++ Eventos-PUC/Hackatruck/3 - VARIÁVEIS E CONSTANTES/0 - pizzaria.swift
@@ -0,0 +1,11 @@
+var saborPizza = "Calabresa" //Variável
+var tamanhoEmCM = 48 //Variável Númerica
+
+let nomePizzaria = "PizzaForDevs" // Constante
+let precoUnico = 49.99 // Constante Númerica
+
+
+print(saborPizza)
+print(tamanhoEmCM)
+print(nomePizzaria)
+print(precoUnico)
