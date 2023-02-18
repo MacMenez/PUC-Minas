4c2ac4d35b20ecb29b856c42c3c9a1b70de4c915
David Ram Menez <dcm.facul@gmail.com>
2023-02-18 11:38:28 -0300

    Testando Nomenclaturas em Swift
    Programa de treinamento do Evento Hackatruck PUC Minas 2023.

---
+++ Eventos-PUC/Hackatruck/4 - NOMENCLATURAS DE VARIÁVEIS E CONSTANTES/0 - nomesDeVariaveis.swift
@@ -0,0 +1,13 @@
+var _teste = 123
+var π = 3.14
+var 🍇🍋🍓 = "Frutas" //Caracteres unicode
+var nome101 = "Joazinho"
+
+/*
+    REGRAS
+    - Nomes não podem conter espaços em branco, 
+    símbolos matemáticos, setas ou ainda caracteres de desenho de linha.
+
+    Nomes de variáveis e constantes também não podem começar com números, 
+    mas os mesmos, como no exemplo acima, podem estar contidos após o primeiro caractere.
+*/
