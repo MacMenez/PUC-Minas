29ed89873f4f090cbc695cc2aabecb28fc11257c
David Ram Menez <dcm.facul@gmail.com>
2023-02-18 15:55:15 -0300

    Testando Comandos de Atribuição em Swift
    Programa de treinamento do Evento Hackatruck PUC Minas 2023.

---
+++ Eventos-PUC/Hackatruck/6 - COMANDOS DE ATRIBUIÇÃO/0 - comandosDeAtribuicao.swift
@@ -0,0 +1,13 @@
+var nome: String = "João"
+
+var sobrenome: String = "Almeida"
+
+var nomeCompleto: String = nome + " " + sobrenome
+
+print (nomeCompleto)
+
+/*
+    Utilizamos + para fazer a concatenação (junção) de Strings.
+    Observe que colocamos  + “  ” +  para adicionar uma String com 
+    espaço entre as Strings nome e sobrenome.
+*/
---
+++ Eventos-PUC/Hackatruck/6 - COMANDOS DE ATRIBUIÇÃO/4 - exercicio_Atribuicao-2.swift
@@ -0,0 +1,32 @@
+//Agora, atribua utilizando as informações contidas nas variáveis:
+
+/******************* 
+Valores à serem utilizados
+********************/
+var dataDoSaque: String  = "01/01/2021"
+var nomeBanco: String = "Money Bank"
+var valorParaSacar: Double = 700
+
+/******************* 
+Variáveis à serem atualizadas
+********************/
+
+var data: String = ""
+var banco: String = ""
+var valorSaque: Double = 0
+
+/******************* 
+Altere os valores aqui 
+********************/
+
+data = "01/01/2021"
+banco = "Money Bank"
+valorSaque = 700
+
+/********************
+Imprima os novos valores aqui 
+********************/
+
+print(data)
+print(banco)
+print(valorSaque)
