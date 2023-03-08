a52a1a45697ed7117ebfc3434ac087c7975c5e84
David Ram Menez <dcm.facul@gmail.com>
2023-03-08 18:39:37 -0300

    Adicionando Código Assembly
    Inserindo atividades em Linguagem Assembly

---
+++ ARQUITETURA-DE-COMPUTADORES-II/Laboratório - ARQ II/Linguagem Assembly/5 - Primeiro Programa (Assembly).asm
@@ -0,0 +1,10 @@
+#declaraçao
+#$zero, $0
+
+.text #delimitar que será porção de códgo 
+.globl main #indica aonde o programa começa
+
+main: #rótulo ou label
+#inicio
+add $s0, $s1, $s2 # f = g + h
+#fim
