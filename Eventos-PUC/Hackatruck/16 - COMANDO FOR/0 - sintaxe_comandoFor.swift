018ebee622ed308ca3e212ee6a2e35b14a64b9fa
David Ram Menez <dcm.facul@gmail.com>
2023-03-14 16:05:46 -0300

    Testando Comando FOR em Swift
    Programa de treinamento do Evento Hackatruck PUC Minas 2023.

---
+++ Eventos-PUC/Hackatruck/16 - COMANDO FOR/0 - sintaxe_comandoFor.swift
@@ -0,0 +1,5 @@
+For variavel in contador {
+
+//Comando a ser executado em todas repetições até que acabe o contador definido
+
+}
---
+++ Eventos-PUC/Hackatruck/16 - COMANDO FOR/1 - exemploFor.swift
@@ -0,0 +1,5 @@
+//Bom dia 5x
+
+for i in 1...5 {
+    print("Bom dia")
+}
