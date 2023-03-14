85325974631bd59ce8c5b2eb00355294f23175af
David Ram Menez <dcm.facul@gmail.com>
2023-03-14 11:58:50 -0300

    Testando Comando WHILE em Swift
    Programa de treinamento do Evento Hackatruck PUC Minas 2023.

---
+++ Eventos-PUC/Hackatruck/14 - COMANDO WHILE/0 - sintaxe_comandoWhile.swift
@@ -0,0 +1,5 @@
+while condicao {
+
+//Comandos a serem executados em todas repetições enquanto a condição for verdadeira
+
+}
---
+++ Eventos-PUC/Hackatruck/14 - COMANDO WHILE/3 - exemploWhile2.swift
@@ -0,0 +1,8 @@
+//CONDIÇÃO FALSA NO INÍCIO
+
+var x = 5
+var y = 5
+ 
+while x < y {
+    print("x é menor que y")
+}
