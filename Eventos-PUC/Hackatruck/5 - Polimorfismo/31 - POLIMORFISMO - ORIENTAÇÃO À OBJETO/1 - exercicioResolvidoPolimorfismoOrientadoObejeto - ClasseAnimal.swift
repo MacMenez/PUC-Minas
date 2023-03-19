2f80d7866b72281e47ef8c37c3ffd48b7b0c25e3
David Ram Menez <dcm.facul@gmail.com>
2023-03-19 13:13:07 -0300

    Estudando ENCAPSULAMENTO, HERANÇA e POLIMORFISMO - ORIENTAÇÃO A OBJETOS em Swift
    Programa de treinamento do Evento Hackatruck PUC Minas 2023.

---
+++ Eventos-PUC/Hackatruck/5 - Polimorfismo/31 - POLIMORFISMO - ORIENTAÇÃO À OBJETO/1 - exercicioResolvidoPolimorfismoOrientadoObejeto - ClasseAnimal.swift
@@ -0,0 +1,4 @@
+//Crie as classes  Animal, Cao, Gato, faça as devidas associações, 
+//e use os conceitos aprendidos de Overloading, e Overriding, 
+//para criar os métodos emitirSom( ), comer( ), escalar( ) e babar ( ).
+
