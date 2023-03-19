50734073239c3c8568b731fd26568796828c3e6f
David Ram Menez <dcm.facul@gmail.com>
2023-03-19 12:12:53 -0300

    Estudando Propriedades(Classe) em Swift
    Programa de treinamento do Evento Hackatruck PUC Minas 2023.

---
+++ Eventos-PUC/Hackatruck/23 - PROPRIEDADES/0 - sintaxeDePropriedades(Classe).swift
@@ -0,0 +1,11 @@
+class Carro {
+    
+// 1
+    var ano: Int? // Estas são algumas das propriedades da classe Carro
+    var marca: String?
+    var modelo: String?
+    var versao: String?
+    var cor: String?
+
+}
+
