425ef146469a3b5cd67cb51863cfeab7283a4945
David Ram Menez <dcm.facul@gmail.com>
2023-03-19 12:26:37 -0300

    Estudando Classes com Orientação À Objetos em Swift
    Programa de treinamento do Evento Hackatruck PUC Minas 2023.

---
+++ Eventos-PUC/Hackatruck/25 - CLASSES - ORIENTAÇÃO A OBJETOS/1 - criandoClasses-Carro.swift
@@ -0,0 +1,15 @@
+class Carro {
+
+    var ano: Int? // Estas são algumas das propriedades da classe Carro
+    var marca: String?
+    var modelo: String?
+    var versao: String?
+    var cor: String?
+    
+    func descricao(){
+        print("O carro \(self.modelo!) da marca \(self.marca!), versao \(self.versao!) e ano \(self.ano!), é da cor \(self.cor!)")
+    }
+
+}
+
+
