dfd14a47630022a89f9546a6acf8be336802353d
David Ram Menez <dcm.facul@gmail.com>
2023-03-19 12:08:07 -0300

    Estudando  em Swift
    Programa de treinamento do Evento Hackatruck PUC Minas 2023.

---
+++ Eventos-PUC/Hackatruck/22 - OBJETOS/0 - sintaxeObejetos.swift
@@ -0,0 +1,7 @@
+
+class Carro{
+     // Seus atributos e metodos podem ser definidos aqui. 
+}
+
+let saveiro  = Carro () // criamos um objeto de Person
+let gol  = Carro () // criamos um objeto de Person
