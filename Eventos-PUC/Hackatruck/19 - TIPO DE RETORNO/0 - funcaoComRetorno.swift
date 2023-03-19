ed7453e9e4e364d3d541d557ef5af7a75f51ccab
David Ram Menez <dcm.facul@gmail.com>
2023-03-19 11:32:12 -0300

    Testando Comando de Retorno de Função em Swift
    Programa de treinamento do Evento Hackatruck PUC Minas 2023.

---
+++ Eventos-PUC/Hackatruck/19 - TIPO DE RETORNO/0 - funcaoComRetorno.swift
@@ -0,0 +1,11 @@
+/* Crie uma função chamada imprimeNome 
+que receba uma String nome, 
+e imprima seu nome quando chamada. */
+func imprimeNome(nome: String) -> String{
+    return "Meu nome é \(nome)"
+}
+
+
+
+
+print(imprimeNome(nome: "Paulo"))
