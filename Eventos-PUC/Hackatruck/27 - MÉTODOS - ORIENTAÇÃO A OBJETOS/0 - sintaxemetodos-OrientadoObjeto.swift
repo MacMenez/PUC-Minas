394867f813f96e91195ad0dd611d8ebd20bd7fbc
David Ram Menez <dcm.facul@gmail.com>
2023-03-19 12:39:16 -0300

    Estudando MÉTODOS - ORIENTAÇÃO A OBJETOS em Swift
    Programa de treinamento do Evento Hackatruck PUC Minas 2023.

---
+++ Eventos-PUC/Hackatruck/27 - MÉTODOS - ORIENTAÇÃO A OBJETOS/0 - sintaxemetodos-OrientadoObjeto.swift
@@ -0,0 +1,22 @@
+//Execução dará erro, exemplo teórico
+
+func nomeDoMetodo() {
+
+    //Corpo do método
+    
+}
+
+func nomeDoMetodo() -> tipoDeDadoDoRetorno {
+
+    //Corpo do método
+    
+}
+
+func nomeDoMetodo (nomeDoParametro : tipoDoParametro) -> tipoDeDadoDoRetorno {
+
+    //Corpo do método
+    
+}
+
+
+//Execução dará erro, exemplo teórico
