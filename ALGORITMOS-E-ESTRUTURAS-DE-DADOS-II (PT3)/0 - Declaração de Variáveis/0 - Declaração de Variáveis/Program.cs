8811c99e5eba78fc3bc1ec7a14572acdfddc105e
David Ram Menez <62731904+MacMenez@users.noreply.github.com>
2023-08-21 12:04:51 -0300

    Add files via upload

---
+++ ALGORITMOS-E-ESTRUTURAS-DE-DADOS-II (PT3)/0 - Declaração de Variáveis/0 - Declaração de Variáveis/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _0___Declaração_de_Variáveis{
+    class Program{
+        static void Main(string[] args){
+            /*
+            DELCARAÇÃO DE VARIÁVEIS EM C#
+
+            < tipo da variável> < nome da variável>;
+            */
+            
+            int x;
+            double y;
+            char c;
+            
+            //O C# também permite múltiplas declarações
+            int a, b;
+            double x1, y1, z1;
+            char c1, c2, c3, c4, c5, c6;
+
+        }
+    }
+}
