37eaca1b5b152be79152bd37d8d244e97c066bc3
David Ram Menez <62731904+MacMenez@users.noreply.github.com>
2023-08-21 12:06:38 -0300

    Estudando Linguagem C#

---
+++ ALGORITMOS-E-ESTRUTURAS-DE-DADOS-II (PT3)/Linguagem de Programação C#/2 - Comando de Entrada/2 - Comando de Entrada/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _2___Comando_de_Entrada{
+    class Program{
+        static void Main(string[] args){
+            int idade = int.Parse(Console.ReadLine());
+            float altura = float.Parse(Console.ReadLine());
+            double temperatura = double.Parse(Console.ReadLine());
+            char sexo = char.Parse(Console.ReadLine());
+            string nome = Console.ReadLine();
+        }
+    }
+}
