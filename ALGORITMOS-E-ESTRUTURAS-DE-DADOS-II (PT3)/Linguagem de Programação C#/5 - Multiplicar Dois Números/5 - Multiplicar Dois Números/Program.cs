bbfc97ccb0d81bcaab9636430d666fa936f67a72
David Ram Menez <62731904+MacMenez@users.noreply.github.com>
2023-08-21 12:09:13 -0300

    Estudando Linguagem C#

---
+++ ALGORITMOS-E-ESTRUTURAS-DE-DADOS-II (PT3)/Linguagem de Programação C#/5 - Multiplicar Dois Números/5 - Multiplicar Dois Números/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _5___Multiplicar_Dois_Números{
+    class Program{
+        static void Main(string[] args){
+            //Declaracao de variáveis.
+            int numero1, numero2, multiplicacao;
+
+            /*Leitura do primeiro numero.*/
+            Console.WriteLine("Digite um número:");
+            numero1 = int.Parse(Console.ReadLine());
+
+            //Leitura do segundo numero.
+            Console.WriteLine("Digite outro número:");
+            numero2 = int.Parse(Console.ReadLine());
+
+            //Multiplicar os dois números
+            multiplicacao = numero1 * numero2;
+
+            //Imprimir na tela o resultado da soma.
+            Console.WriteLine("Multiplicação:" + multiplicacao);
+        }
+    }
+}
