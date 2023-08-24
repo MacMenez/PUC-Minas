bd3b7dcfd6be58df087a14172d189d9222b4401b
David Ram Menez <62731904+MacMenez@users.noreply.github.com>
2023-08-24 11:55:47 -0300

    Estacionamento
    Código responsável por gerir um estacionamento e administrar entrada, saída e consulta local ao estacionamento. Utilizado classe e valores informados no enunciado da questão.

---
+++ ALGORITMOS-E-ESTRUTURAS-DE-DADOS-II (PT3)/Algoritmos e Estruturas de Dados II - LAB/Lista 3 - Nivelamento – Introdução a Programação por Objetos e Arquivo Texto/Lista3_ATVD1/Lista3_ATVD1/Estacionamento.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Lista3_ATVD1{
+    class Estacionamento{
+        private String nome; // nome do estacionamento.
+        private int numTotalVagas; // número total de vagas que o estacionamento apresenta.
+        private int numVagasLivres; // número de vagas livres no estacionamento.
+        private String[] vaga; // vetor que armazena cada uma das vagas do estacionamento.Caso a vaga esteja ocupada, este vetor indica a placa do veículo que a ocupa.
+        
+        public Estacionamento(String nome, int numTotalVagas){
+            this.nome = nome;
+            this.numTotalVagas = numTotalVagas;
+            this.numVagasLivres = numTotalVagas;
+            vaga = new String[numTotalVagas];
+        }
+
+        public int Estacionar(String placa){
+            int posicao = -1;
+            for (int i = 0; i < vaga.Length; i++){
+                if (vaga[i] == null){
+                    vaga[i] = placa;
+                    posicao = i; //Posição que o carro foi estacionado
+                    i = vaga.Length;
+                }
+            }
+            return posicao;
+        }
+        public int ObterVagaOcupada(String placa){
+            for (int i = 0; i < vaga.Length; i++){
+                if (vaga[i] == placa){
+                    return i;
+                }
+            }
+            return -1;
+        }
+        public void RetirarVeiculo(String placa) {
+            for (int i = 0; i < vaga.Length; i++){
+                if (vaga[i] == placa){
+                    vaga[i] = null;
+                }
+            }
+        }
+        public int ObterNumVagasLivres(){
+            for (int i = 0; i < vaga.Length; i++){
+                if (vaga[i] == null){
+                    numVagasLivres++;
+                }
+            }
+            return numVagasLivres;
+        }
+        public void ExibirOcupacaoEstacionamento(){
+            for (int i = 0; i < vaga.Length; i++){
+                Console.WriteLine("Vaga {0}: {1}", i, vaga[i]);
+            }
+        }
+
+    }
+}
---
+++ ALGORITMOS-E-ESTRUTURAS-DE-DADOS-II (PT3)/Algoritmos e Estruturas de Dados II - LAB/Lista 3 - Nivelamento – Introdução a Programação por Objetos e Arquivo Texto/Lista3_ATVD1/Lista3_ATVD1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Lista3_ATVD1{
+    class Program{
+        static void Main(string[] args){
+            int vagaOcupada;
+            Estacionamento objEstacionamento = new
+           Estacionamento("Estacionamento de AEDs", 10);
+            vagaOcupada = objEstacionamento.Estacionar("HKT0098");
+            vagaOcupada = objEstacionamento.Estacionar("OLP4290");
+            vagaOcupada = objEstacionamento.Estacionar("HJB0495");
+            vagaOcupada = objEstacionamento.Estacionar("OWB3904");
+            Console.WriteLine("Ocupação do estacionamento após as chegadas de quatro clientes: ");
+
+
+            objEstacionamento.ExibirOcupacaoEstacionamento(); // HKT0098 OLP4290 HJB0495 OWB3904 vazia vazia vazia vazia vazia vazia
+             vagaOcupada = objEstacionamento.ObterVagaOcupada("HKT0098");
+            Console.WriteLine("Veículo de placa HKT0098 estacionado na vaga " + vagaOcupada); // 0
+            objEstacionamento.RetirarVeiculo("HKT0098");
+            Console.WriteLine("Ocupação do estacionamento após a retirada do veículo de placa HKT0098: ");
+
+
+            objEstacionamento.ExibirOcupacaoEstacionamento(); // vazia OLP4290 HJB0495 OWB3904 vazia vazia vazia vazia vazia vazia
+            vagaOcupada = objEstacionamento.Estacionar("HTP5619");
+            vagaOcupada = objEstacionamento.Estacionar("BOL4861");
+            vagaOcupada = objEstacionamento.Estacionar("HGT9436");
+            Console.WriteLine("Ocupação do estacionamento após as chegadas de mais três clientes: ");
+
+
+            objEstacionamento.ExibirOcupacaoEstacionamento(); // HTP5619 OLP4290 HJB0495 OWB3904 BOL4861 HGT9436 vazia vazia vazia vazia
+            Console.WriteLine("Este estacionamento apresenta {0} vagas livres.", objEstacionamento.ObterNumVagasLivres()); // 4 vagas livres
+            
+            Console.ReadLine();
+        }
+
+    }
+}
