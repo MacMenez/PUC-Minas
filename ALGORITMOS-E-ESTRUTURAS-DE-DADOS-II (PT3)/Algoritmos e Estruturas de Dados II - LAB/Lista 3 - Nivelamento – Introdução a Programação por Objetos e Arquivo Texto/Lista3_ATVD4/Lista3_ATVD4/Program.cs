57ed01b516597447030a2665bc6a2fda3126150c
David Ram Menez <62731904+MacMenez@users.noreply.github.com>
2023-08-29 20:56:23 -0300

    Sistema Para Controle da Biblioteca Pessoal
    Realizando testes após alterações

--- ALGORITMOS-E-ESTRUTURAS-DE-DADOS-II (PT3)/Algoritmos e Estruturas de Dados II - LAB/Lista 3 - Nivelamento – Introdução a Programação por Objetos e Arquivo Texto/Lista3_ATVD4/Lista3_ATVD4/Program.cs
+++ ALGORITMOS-E-ESTRUTURAS-DE-DADOS-II (PT3)/Algoritmos e Estruturas de Dados II - LAB/Lista 3 - Nivelamento – Introdução a Programação por Objetos e Arquivo Texto/Lista3_ATVD4/Lista3_ATVD4/Program.cs
@@ -1,10 +1,16 @@
-﻿using System;
+using System;
 
 namespace Lista3_ATVD4{
     class Program{
         //Sistema Para Controle da Biblioteca Pessoal
         static void Main(string[] args){
-            Console.WriteLine("Hello World!");
+            Biblioteca biblioteca = new Biblioteca();
+            biblioteca.AdicionarLivro();
+            biblioteca.AdicionarLivro();
+            biblioteca.AdicionarLivro();
+            biblioteca.AdicionarLivro();
+
+            biblioteca.LocalizarLivro("Livro1");
         }
     }
 }
