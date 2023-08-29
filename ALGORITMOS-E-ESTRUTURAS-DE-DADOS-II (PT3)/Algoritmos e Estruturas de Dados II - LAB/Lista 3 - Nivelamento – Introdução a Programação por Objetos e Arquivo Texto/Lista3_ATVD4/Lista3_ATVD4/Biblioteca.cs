83132610ae1ebf9011aaf78eda596efe3228ad7d
David Ram Menez <62731904+MacMenez@users.noreply.github.com>
2023-08-29 20:57:56 -0300

    Sistema Para Controle da Biblioteca Pessoal
    Finalizando código e preparando para realizar testes. Há erros evidentes que serão corrigidos no futuro.

--- ALGORITMOS-E-ESTRUTURAS-DE-DADOS-II (PT3)/Algoritmos e Estruturas de Dados II - LAB/Lista 3 - Nivelamento – Introdução a Programação por Objetos e Arquivo Texto/Lista3_ATVD4/Lista3_ATVD4/Biblioteca.cs
+++ ALGORITMOS-E-ESTRUTURAS-DE-DADOS-II (PT3)/Algoritmos e Estruturas de Dados II - LAB/Lista 3 - Nivelamento – Introdução a Programação por Objetos e Arquivo Texto/Lista3_ATVD4/Lista3_ATVD4/Biblioteca.cs
@@ -1,9 +1,39 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Lista3_ATVD4{
     class Biblioteca{
+        private Livro[] livros;
+      private int numLivros;
+      private int maxLivros = 50;
 
+      public void AdicionarLivro(String titulo, String autores, String editora){
+        if(numLivros < maxLivros){
+          livros[numLivros].SetTitulo(titulo);
+          livros[numLivros].SetAutores(autores);
+          livros[numLivros].SetEditora(editora);
+          numLivros++;
+        }
+      }
+
+      public void AdicionarLivro(Livro livro){
+        livro[numLivros] = livro;
+      }
+
+      public LocalizarLivro(String nomeLivro){
+        for(int i = 0; i < livros.Length; i++){
+          if(livros[i].GetTitulo == nomeLivro){
+            return livros[i];
+          }else{
+            Console.WriteLine("Livro Não Existe!");
+            return null;
+          }
+        }
+      }
+
+      public Livro ListaLivros(){return livros;}
+
+      public int TotalLivros(){return numLivros;}
     }
 }
