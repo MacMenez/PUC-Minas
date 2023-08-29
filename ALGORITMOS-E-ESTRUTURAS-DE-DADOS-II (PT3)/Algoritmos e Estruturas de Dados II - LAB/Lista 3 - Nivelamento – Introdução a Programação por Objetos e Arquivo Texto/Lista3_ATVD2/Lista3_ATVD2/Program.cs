5a7ffea2e09fdbb39694bf552f88c55102450a44
David Ram Menez <62731904+MacMenez@users.noreply.github.com>
2023-08-29 12:13:35 -0300

    Cálculo do IMC
    Necessário corrigir algumas pendências do código e verificar se o proposto foi comprido corretamente.

---
+++ ALGORITMOS-E-ESTRUTURAS-DE-DADOS-II (PT3)/Algoritmos e Estruturas de Dados II - LAB/Lista 3 - Nivelamento – Introdução a Programação por Objetos e Arquivo Texto/Lista3_ATVD2/Lista3_ATVD2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lista3_ATVD2
+{
+    class Program{
+        static void Main(string[] args){
+
+            Calendario calendario = new Calendario(28, 08, 2023);
+
+            Pessoa pessoa = new Pessoa(23, 75.10, 1.75);
+
+            pessoa.CalcularIMC;
+            pessoa.InformarIdade;
+        }
+    }
+}
