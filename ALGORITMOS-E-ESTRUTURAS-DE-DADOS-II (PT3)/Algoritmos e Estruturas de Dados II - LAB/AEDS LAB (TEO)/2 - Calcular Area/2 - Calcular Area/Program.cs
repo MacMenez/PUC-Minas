7f03fbb65b0f7b08a408f08650d5788514a93dd9
David Ram Menez <62731904+MacMenez@users.noreply.github.com>
2023-09-01 08:31:31 -0300

    Calculadora de Área
    Utilização de classe para calcular área de determinada medida informada pelo usuário

---
+++ ALGORITMOS-E-ESTRUTURAS-DE-DADOS-II (PT3)/Algoritmos e Estruturas de Dados II - LAB/AEDS LAB (TEO)/2 - Calcular Area/2 - Calcular Area/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _2___Calcular_Area{
+    class Program{
+        static void Main(string[] args){
+            
+            double l;
+            
+            Console.WriteLine("Informe o lado do quadrado:");
+            l = double.Parse(Console.ReadLine());
+            //Instanciando quadrado 1
+            Quadrado q1 = new Quadrado(l);
+            
+            Console.WriteLine("Informe o lado do quadrado:");
+            l = double.Parse(Console.ReadLine());
+            //Instanciando quadrado 2
+            Quadrado q2 = new Quadrado(l);
+            
+            double area1 = q1.CalcularArea();
+            
+            Console.WriteLine("Area do quadrado 1: " + area1);
+            l = q2.GetLado();
+            
+            Console.WriteLine("Lado do quadrado 2 " + l);
+            q2.SetLado(10);
+            
+            l = q2.GetLado();
+            
+            Console.WriteLine("Lado do quadrado 2 " + l);
+            
+            Console.ReadLine();
+
+            /*
+            //Demostrativo utilizando a outra forma de declaração de metodos
+            double area1 = q1.CalcularArea();
+            Console.WriteLine("Area do quadrado 1: " + area1);
+            l = q2.Lado;
+            Console.WriteLine("Lado do quadrado 2 " + l);
+            q2.Lado = 10;
+            l = q2.Lado;
+            Console.WriteLine("Lado do quadrado 2 " + l);
+            */
+        }
+    }
+}
