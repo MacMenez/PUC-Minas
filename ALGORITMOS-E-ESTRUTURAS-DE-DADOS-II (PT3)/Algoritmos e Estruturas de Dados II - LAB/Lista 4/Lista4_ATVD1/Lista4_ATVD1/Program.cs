bc07c3cd2784ed66bace7c3b44d11fd75ce75df1
David Ram Menez <62731904+MacMenez@users.noreply.github.com>
2023-09-11 12:11:20 -0300

    Potência de Base

---
+++ ALGORITMOS-E-ESTRUTURAS-DE-DADOS-II (PT3)/Algoritmos e Estruturas de Dados II - LAB/Lista 4/Lista4_ATVD1/Lista4_ATVD1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Lista4_ATVD1{
+    class Program{
+        static void Main(string[] args){
+            /*ATIVIDADE 1*/
+            //Console.WriteLine(Recursividade(2, 5));
+
+            /*ATIVIDADE 2*/
+            //Console.WriteLine(SomaNumeroIntervalo(4, 7));
+
+            /*ATIVIDADE 3*/
+            //Console.WriteLine(ConverterParaBinario(12));
+            //Console.WriteLine(ConverterParaBinario(8));
+
+            /*ATIVIDADE 4*/
+            //Console.WriteLine(MDC(10, 6));
+        }
+
+        public static int MDC(int x, int y){
+            return 1;
+        }
+
+        public static int ConverterParaBinario(int x){
+            if (x / 2 >= 0){
+                Console.WriteLine(x % 2);
+                return ConverterParaBinario(x / 2);
+            }
+            else{
+                return 1;
+            }
+        }
+
+        public static int SomaNumeroIntervalo(int m, int n){
+            if (m <= n){
+                m++;
+                return SomaNumeroIntervalo(m, n);
+            }
+            else{
+                return -1;
+            }
+            
+        }
+
+        public static int Recursividade(int a, int n){
+            /* a = 2
+             * n = 5
+             */
+            if (n >= 0){
+                n--;
+            }
+            return Recursividade(a * a, n--);
+        }
+    }
+}
