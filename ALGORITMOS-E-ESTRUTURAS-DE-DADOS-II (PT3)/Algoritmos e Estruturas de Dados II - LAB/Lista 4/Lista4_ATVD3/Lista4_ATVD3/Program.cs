2e250469886cfb453c1cbf40c63c32783b097521
David Ram Menez <62731904+MacMenez@users.noreply.github.com>
2023-09-11 12:12:38 -0300

    Conversor de Número Decimal Para Binário

---
+++ ALGORITMOS-E-ESTRUTURAS-DE-DADOS-II (PT3)/Algoritmos e Estruturas de Dados II - LAB/Lista 4/Lista4_ATVD3/Lista4_ATVD3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lista4_ATVD3{
+    class Program{
+        static void Main(string[] args){
+            /*ATIVIDADE 3*/
+            //Console.WriteLine(ConverterParaBinario(12));
+            //Console.WriteLine(ConverterParaBinario(8));
+        }
+
+        public static int ConverterParaBinario(int x){
+            if (x / 2 >= 0){
+                Console.WriteLine(x % 2);
+                return ConverterParaBinario(x / 2);
+            }
+            else{
+                return 1;
+            }
+        }
+    }
+}
