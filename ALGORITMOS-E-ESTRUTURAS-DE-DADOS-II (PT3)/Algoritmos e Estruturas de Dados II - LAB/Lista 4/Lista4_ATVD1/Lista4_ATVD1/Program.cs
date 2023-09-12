f1f7a5914c17701c5a5da5beab150f27b70ad302
David Ram Menez <62731904+MacMenez@users.noreply.github.com>
2023-09-12 01:15:14 -0300

    Potência de Base

--- ALGORITMOS-E-ESTRUTURAS-DE-DADOS-II (PT3)/Algoritmos e Estruturas de Dados II - LAB/Lista 4/Lista4_ATVD1/Lista4_ATVD1/Program.cs
+++ ALGORITMOS-E-ESTRUTURAS-DE-DADOS-II (PT3)/Algoritmos e Estruturas de Dados II - LAB/Lista 4/Lista4_ATVD1/Lista4_ATVD1/Program.cs
@@ -2,54 +2,63 @@
 
 namespace Lista4_ATVD1{
     class Program{
-        static void Main(string[] args){
-            /*ATIVIDADE 1*/
-            //Console.WriteLine(Recursividade(2, 5));
+        public static void Main (string[] args) {
+    /*ATIVIDADE 1*/
+            Console.WriteLine(Recursividade(2, 5));
 
             /*ATIVIDADE 2*/
-            //Console.WriteLine(SomaNumeroIntervalo(4, 7));
+            Console.WriteLine(SomaNumeroIntervalo(4, 7));
 
             /*ATIVIDADE 3*/
-            //Console.WriteLine(ConverterParaBinario(12));
-            //Console.WriteLine(ConverterParaBinario(8));
+            ConverterParaBinario(12);
+            Console.WriteLine("\n");
+            ConverterParaBinario(8);
 
             /*ATIVIDADE 4*/
-            //Console.WriteLine(MDC(10, 6));
+            Console.WriteLine("\n" + MDC(10, 6));
+  }
+  public static int MDC(int x, int y) {
+        // x = y, o MDC é x (ou y. MDC(x, y) = MDC(y, x))
+        if (x == y) {
+            return x;
         }
 
-        public static int MDC(int x, int y){
+        // x > y
+        if (x > y) {
+            return MDC(x - y, y);
+        }
+
+        // y < x
+        return MDC(x, y - x);
+    }
+
+         public static void ConverterParaBinario(int x) {
+        if (x > 0) {
+          // Recursiva com x / 2
+            ConverterParaBinario(x / 2); 
+            Console.Write(x % 2);
+        }
+    }
+
+        public static int SomaNumeroIntervalo(int m, int n) {
+        if (m <= n) {
+          //Soma m e chama recursivamente com m incrementado.
+            return m + SomaNumeroIntervalo(m + 1, n);
+        } else {
+          // m > n, retorna 0.
+            return 0; 
+        }
+    }
+
+        public static int Recursividade(int a, int n) {
+          // Flag: n = 0, retorna 1.
+          if (n == 0) {
             return 1;
-        }
-
-        public static int ConverterParaBinario(int x){
-            if (x / 2 >= 0){
-                Console.WriteLine(x % 2);
-                return ConverterParaBinario(x / 2);
-            }
-            else{
-                return 1;
-            }
-        }
-
-        public static int SomaNumeroIntervalo(int m, int n){
-            if (m <= n){
-                m++;
-                return SomaNumeroIntervalo(m, n);
-            }
-            else{
-                return -1;
-            }
-            
-        }
-
-        public static int Recursividade(int a, int n){
-            /* a = 2
-             * n = 5
-             */
-            if (n >= 0){
-                n--;
-            }
-            return Recursividade(a * a, n--);
+        } else if (n > 0) {
+            // Recursiva com n decrementado.
+            return a * Recursividade(a, n - 1); 
+        } else {
+            throw new ArgumentException("O valor de n deve ser não negativo.");
         }
     }
 }
