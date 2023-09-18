201b3c13c4248001af0b27b16f9811e2ffe98671
David Ram Menez <62731904+MacMenez@users.noreply.github.com>
2023-09-18 12:13:24 -0300

    Números Naturais de 0 até N em Ordem Crescente

---
+++ ALGORITMOS-E-ESTRUTURAS-DE-DADOS-II (PT3)/Algoritmos e Estruturas de Dados II - LAB/Lista 5 - Recursividade PT2/NumerosNaturais/NumerosNaturais/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NumerosNaturais{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine(NumerosNaturais(5));
+        }
+
+        public static int NumerosNaturais(int n){
+            if (n <= 0){
+                n = 1 + NumerosNaturais(n - 1);
+            }
+            return n;
+        }
+    }
+}
