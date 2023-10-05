9f5dcd4255e8904515333433a2b6ccc2ada477ab
David Ram Menez <62731904+MacMenez@users.noreply.github.com>
2023-10-05 11:05:32 -0300

    Notação Polonesa com Vetor De Pilha

---
+++ ALGORITMOS-E-ESTRUTURAS-DE-DADOS-II (PT3)/Algoritmos e Estruturas de Dados II - LAB/Lista7/NotacaoPolonesaVetorDePilha/NotacaoPolonesaVetorDePilha/Pilha.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotacaoPolonesaVetorDePilha{
+    class Pilha{
+        
+        private double[] vetor;
+
+        private int tamanho;
+
+        public int Tamanho{
+            get { return tamanho; }
+            set { tamanho = value; }
+        }
+
+
+        public Pilha(int capacidade){
+            tamanho = 0;
+            vetor = new double[capacidade];
+        }
+
+        public void Empilhar(double valor){
+            if (tamanho < vetor.Length){
+                vetor[tamanho] = valor;
+                tamanho++;
+            }
+            else{
+                Console.WriteLine("Erro: Pilha cheia.");
+            }
+        }
+
+        public double Desempilhar(){
+            if (tamanho > 0){
+                tamanho--;
+                return vetor[tamanho];
+            }
+            else{
+                Console.WriteLine("Erro: Pilha vazia.");
+                return double.NaN;
+            }
+        }
+
+        public bool EstaVazia(){
+            return tamanho == 0;
+        }
+    }
+}
