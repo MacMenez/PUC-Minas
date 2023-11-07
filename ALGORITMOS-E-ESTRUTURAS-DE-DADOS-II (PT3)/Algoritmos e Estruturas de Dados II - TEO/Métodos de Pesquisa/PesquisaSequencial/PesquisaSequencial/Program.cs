bbc8af656c9459357420d76a525482023a05b80a
David Ram Menez <62731904+MacMenez@users.noreply.github.com>
2023-11-07 12:48:53 -0300

    Pesquisa Sequencial

---
+++ ALGORITMOS-E-ESTRUTURAS-DE-DADOS-II (PT3)/Algoritmos e Estruturas de Dados II - TEO/Métodos de Pesquisa/PesquisaSequencial/PesquisaSequencial/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PesquisaSequencial{
+    class Program{
+        public static Boolean pesquisaSequencial(int[] vet, int x){
+            Boolean resp = false;
+            int n = vet.Length;
+
+            for (int i = 0; i < n; i++){
+                if (vet[i] == x){
+                    resp = true;
+                    i = n;
+                }
+            }
+            return resp;
+        }
+
+        static void Main(string[] args){
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
