4100c85f3dbce4c0cc16ddf30cc3c3116e50134b
David Ram Menez <62731904+MacMenez@users.noreply.github.com>
2023-11-09 10:15:33 -0300

    Métodos de Ordenação - BubbleSort

---
+++ ALGORITMOS-E-ESTRUTURAS-DE-DADOS-II (PT3)/Algoritmos e Estruturas de Dados II - TEO/Métodos de Ordenação/BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BubbleSort{
+    class Program{
+        public static void bubblesort(int[] array, int n){
+            for (int i = (n - 1); i > 0; i--){
+                for (int j = 0; j < i; j++){
+                    if (array[j] > array[j + 1]){
+
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+
+            foreach (int item in array){
+                Console.Write(item + " - ");
+            }
+        }
+
+        static void Main(string[] args){
+            int[] array = new int[5] { 80, 16, 10, 26, 28 };
+            
+            foreach (int item in array){
+                Console.Write(item + " - ");
+            }
+
+            Console.WriteLine("\nVetor Ordenado com o Método de Ordenação BubbleSort!");
+            bubblesort(array, 5);
+        }
+    }
+}
