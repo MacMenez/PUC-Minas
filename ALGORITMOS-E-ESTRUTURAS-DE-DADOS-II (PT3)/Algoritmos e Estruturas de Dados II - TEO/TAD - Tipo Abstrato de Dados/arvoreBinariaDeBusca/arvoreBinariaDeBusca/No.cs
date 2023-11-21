26abe98994ff77e0e653ec33a5d37237ac3ee924
David Ram Menez <62731904+MacMenez@users.noreply.github.com>
2023-11-21 11:26:11 -0300

    Árvore Binária De Busca

---
+++ ALGORITMOS-E-ESTRUTURAS-DE-DADOS-II (PT3)/Algoritmos e Estruturas de Dados II - TEO/TAD - Tipo Abstrato de Dados/arvoreBinariaDeBusca/arvoreBinariaDeBusca/No.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace arvoreBinariaDeBusca{
+    class No{
+
+        private Inteiro item;
+        public Inteiro Item{
+            get { return item; }
+            set { item = value; }
+        }
+
+        private No esquerda;
+        public No Esquerda{
+            get { return esquerda; }
+            set { esquerda = value; }
+        }
+
+        private No direita;
+        public No Direita{
+            get { return direita; }
+            set { direita = value; }
+        }
+
+        public No(){
+            item = new Inteiro();
+            esquerda = null;
+            direita = null;
+        }
+
+        public No(Inteiro registro){
+            item = registro;
+            esquerda = null;
+            direita = null;
+        }
+    }
+}
