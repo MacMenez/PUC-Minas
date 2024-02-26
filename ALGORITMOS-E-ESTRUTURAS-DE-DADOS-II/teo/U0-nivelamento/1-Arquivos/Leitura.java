8d72542cd5b2b5370fe6fbca15845db2c9d478b6
David Ram Menez <62731904+MacMenez@users.noreply.github.com>
2024-02-25 22:23:16 -0300

    Arquivos em JAVA - Código de Leitura

--- ALGORITMOS-E-ESTRUTURAS-DE-DADOS-II/teo/U0-nivelamento/1-Arquivos/Leitura.java
+++ ALGORITMOS-E-ESTRUTURAS-DE-DADOS-II/teo/U0-nivelamento/1-Arquivos/Leitura.java
@@ -2,11 +2,13 @@
 class Leitura {
     public static void main (String[] args){     
         Arq.openRead("exemplo.txt");
-            int inteiro = Arq.readInt();
-            double real = Arq.readDouble();
-            char caractere = Arq.readChar();
-            boolean boleano = Arq.readBoolean();
-            String str = Arq.readString();
+
+        int inteiro = Arq.readInt();
+        double real = Arq.readDouble();
+        char caractere = Arq.readChar();
+        boolean boleano = Arq.readBoolean();
+        String str = Arq.readString();
+        
         Arq.close(); 
         
         System.out.println("inteiro: " + inteiro);
