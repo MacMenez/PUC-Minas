1dd9d7c245183467f49f180afe9d949a7fef70a4
David Ram Menez <dcm.facul@gmail.com>
2024-10-08 11:52:37 -0300

    Atualizando mecanismos de leitura e formatação de dados
    Trocado o mecanismo de se criar lista, agora usará ArrayList em tudo e mecanismo de localização de informação foi atualizado para uma melhor leitura e identificação de informações. O método de leitura de dados ainda necessita de modificações. Classes Lista e Data se apresentaram ineficientes e foram deletadas.

--- AEDS II/trabalhos/TP02Q01 - Classe em Java/App.java
+++ AEDS II/trabalhos/TP02Q01 - Classe em Java/App.java
@@ -1,20 +1,19 @@
 import java.io.RandomAccessFile;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         /* CRIAR UMA LISTA DO TIPO OBJETO(POKEMON) */
-        List<Pokemon> listaPokemons = new ArrayList<>();
+        ArrayList<Pokemon> listaPokemons = new ArrayList<Pokemon>();
+
         /* LER ARQUIVOS CSV */
         try {
             RandomAccessFile lerArquivoCSV;
-
+            /* CÓDIGO PARA LEITURA DE ARQUVIVOS CSV NO DIRETÓRIO ATUAL */
             lerArquivoCSV = new RandomAccessFile("pokemon.csv", "r");
 
             /* CÓDIGO PARA LEITURA DE ARQUVIVOS CSV NO SOFTWARE VERDE - PUCMINAS */
-
             // lerArquivoCSV = new RandomAccessFile("/tmp/pokemon.csv", "r");
 
             lerArquivoCSV.readLine(); // Ignorar o cabeçalho do arquivo CSV
@@ -34,23 +33,20 @@ public class App {
                 listaPokemons.add(pokemon); /* Adiciona o objeto pokemon (com seus atributos) na lista de objetos 'listaPokemon' */
             }
 
-
+            lerArquivoCSV.close();
         } catch (Exception e) { System.out.println("Arquivo não encontrado: " + e.getMessage()); }
 
         /* LEITURA DO ARQUIVO PUB.IN que contém os ID do que deve ser mostrado */
         Scanner teclado = new Scanner(System.in);
-            int tamanhoLista = listaPokemons.size(); // Método interno da calsse Java, retorna um inteiro com o total de elementos
-
             String idLocalizacao = teclado.nextLine(); // Leitura do arquivo PUB.IN
 
             while (!idLocalizacao.equals("FIM")) {
                 /* Dado o id lido localizá-lo na lista de objetos e mostrar com o métodos do objeto pokemon da lista de objetos Pokemon */
-                
-                /*for (Pokemon pokemon : listaPokemons) {
-                    if (idLocalizacao.equals(pokemon.getId())) { pokemon.imprimir(); } 
+                int id = Integer.valueOf(idLocalizacao);
+                for (Pokemon p : listaPokemons) {
+                    if (id == p.getId()) { p.imprimir(); } 
                     else { System.out.println("Pokemon não encontrado! Verificar ID"); }
-                }*/
-
+                }
                 idLocalizacao = teclado.nextLine();
             }
         teclado.close();
@@ -58,13 +54,14 @@ public class App {
 }
 
 /*
- * 
- * String dadosPokemon = "1,1,Bulbasaur,Seed Pokémon,grass,poison,\"['Overgrow', 'Chlorophyll']\",6.9,0.7,45,0,05/01/1996";// Variável para ler as linhas do arquivo CSV
-        // CRIAR OBJETO POKEMON
-        Pokemon pokemon = new Pokemon();
+String dadosPokemon = "1,1,Bulbasaur,Seed Pokémon,grass,poison,\"['Overgrow', 'Chlorophyll']\",6.9,0.7,45,0,05/01/1996";
+        
+Pokemon pokemon = new Pokemon(); // CRIAR OBJETO POKEMON
 
-    // A String é tratada na classe Pokemon para dividir os dados para os atributos específicos 
-        pokemon.lerDados(dadosPokemon);
+// A String é tratada na classe Pokemon para dividir os dados para os atributos específicos
+pokemon.lerDados(dadosPokemon);
 
-        listaPokemons.add(pokemon); /* Adiciona o objeto pokemon (com seus atributos) na lista de objetos 'listaPokemon'
+listaPokemons.add(pokemon); //Adiciona o objeto pokemon (com seus atributos) na lista de objetos 'listaPokemon'
  */
+
+ 
