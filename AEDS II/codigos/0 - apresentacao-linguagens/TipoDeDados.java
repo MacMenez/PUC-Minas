634e1e10661c3210ffec9a2ba0046a8f0e70fcea
David Ram Menez <dcm.facul@gmail.com>
2024-10-17 09:36:31 -0300

    Tipos de Dados em Linguagem Java

--- AEDS II/codigos/0 - apresentacao-linguagens/TipoDeDados.java
+++ AEDS II/codigos/0 - apresentacao-linguagens/TipoDeDados.java
@@ -2,4 +2,5 @@ byte, short, int, long //Inteiro
 float, double //ponto flutuante
 char //caractere
 boolean //Booleano
-classe String //Cadeia de Caracteres(vetor)
+String //Cadeia de Caracteres(vetor)
+
