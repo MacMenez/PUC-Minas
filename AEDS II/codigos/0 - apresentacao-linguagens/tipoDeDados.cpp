26063a632fedc419899d3c20ed8e9797b255bc0d
David Ram Menez <dcm.facul@gmail.com>
2024-10-17 09:31:26 -0300

    Tipos de Dados em Linguagem C++

---
+++ AEDS II/codigos/0 - apresentacao-linguagens/tipoDeDados.cpp
@@ -0,0 +1,12 @@
+char //caractere
+int //inteiro
+float //ponto flutuante
+double //ponto flutuante
+bool //boleano
+char[] //cadeia de carcteres(vetor)
+
+// C possue os modificadores de tipos:
+short
+long
+unsigned
+signed
