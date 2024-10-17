a6c013799a94d584d5f3d4a7cb67dd97d767be97
David Ram Menez <dcm.facul@gmail.com>
2024-10-17 09:30:25 -0300

    Tipos de Dados em Linguagem C#

---
+++ AEDS II/codigos/0 - apresentacao-linguagens/tipoDeDados.cs
@@ -0,0 +1,6 @@
+sbyte, short, int, long //Inteiro com Sinal
+byte, ushort, uint, ulong //Inteiro sem Sinal
+char //caractere
+float, double, decimal //Ponto Flutuante
+bool //Boleano
+String //cadeia de caracteres(vetor)
